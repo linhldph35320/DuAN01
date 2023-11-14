995603a1929a1284046e86a81fc32b8541e9427e
XuyenQua <ntx01062004@gmail.com>
2023-11-14 18:08:46 +0700

    update

--- admin/header.php
+++ admin/header.php
@@ -6,7 +6,7 @@
       <!-- Required meta tags -->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Booksto - Responsive Bootstrap 4 Admin Dashboard Template</title>
+      <title> xuyen Booksto - Responsive Bootstrap 4 Admin Dashboard Template</title>
       <!-- Favicon -->
       <link rel="shortcut icon" href="images/favicon.ico" />
       <!-- Bootstrap CSS -->
