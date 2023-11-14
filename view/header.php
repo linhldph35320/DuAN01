01c74cf78c6137c2d0e8669751e419112260b3a2
linhldph35320 <linhldph35320@fpt.edu.vn>
2023-11-14 18:01:26 +0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Linh Junko - Electronics eCommerce HTML Template</title>
+    <title>Linh Linh Junko - Electronics eCommerce HTML Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
