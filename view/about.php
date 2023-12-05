4d7fedf07ed5cda08866934452ab54dcc99cd6a7
linhldph35320 <linhldph35320@fpt.edu.vn>
2023-12-05 16:25:28 +0700

    all code

---
+++ view/about.php
@@ -0,0 +1,162 @@
+<div class="breadcrumbs_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb_content">
+                    <ul>
+                        <li><a href="index.html">Trang chủ</a></li>
+                        <li>Về chúng tôi</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--breadcrumbs area end-->
+
+<!--about section area -->
+<section class="about_section mt-60">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12">
+                <figure>
+                    <div class="about_thumb">
+                        <img src="assets/img/about/about1.jpg" alt="">
+                    </div>
+                    <figcaption class="about_content">
+                        <h1>Điện thoại chính hãng, giá tốt, bảo hành 1 đổi 1</h1>
+                        <p>Nếu bạn đang tìm kiếm một chiếc điện thoại chất lượng tốt, an toàn cho người dùng, bảo hành chính hãng và giá cả hợp lý thì điện thoại chính hãng, đến với chúng tôi là một lựa chọn phù hợp.</p>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+<!--about section end-->
+
+<!--services img area-->
+<div class="about_gallery_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <article class="single_gallery_section">
+                    <figure>
+                        <div class="gallery_thumb">
+                            <img src="assets/img/about/about2.jpg" alt="">
+                        </div>
+                        <figcaption class="about_gallery_content">
+                            <h3>Chúng tôi làm gì?</h3>
+                            <p>Chúng tôi đã và đang không ngừng nỗ lực để mang đến cho khách hàng những sản phẩm và dịch vụ tốt nhất. Công ty luôn cập nhật những xu hướng công nghệ mới nhất để đáp ứng nhu cầu của khách hàng. MWG cũng áp dụng nhiều chính sách bán hàng ưu đãi, giúp khách hàng tiết kiệm chi phí khi mua sắm.</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <article class="single_gallery_section">
+                    <figure>
+                        <div class="gallery_thumb">
+                            <img src="assets/img/about/about3.jpg" alt="">
+                        </div>
+                        <figcaption class="about_gallery_content">
+                            <h3>Lịch sử về chúng tôi</h3>
+                            <p>Với sự nỗ lực của đội ngũ sáng lập, chúng tôi đã nhanh chóng phát triển và trở thành nhà bán lẻ điện thoại di động hàng đầu Việt Nam. Năm 2007, công ty nhận vốn đầu tư của quỹ Mekong Capital.</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
+<!--services img end-->
+
+<!--chose us area start-->
+<div class="choseus_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                <div class="single_chose">
+                    <div class="chose_icone">
+                        <img src="assets/img/about/About_icon1.png" alt="">
+                    </div>
+                    <div class="chose_content">
+                        <h3>Đảm bảo lại tiền</h3>
+                        <p>Nếu như sản phẩm lỗi, chúng tôi hoàn trả 100%.</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="single_chose">
+                    <div class="chose_icone">
+                        <img src="assets/img/about/About_icon2.png" alt="">
+                    </div>
+                    <div class="chose_content">
+                        <h3>Thiết kế sáng tạo</h3>
+                        <p>Chúng tôi sẽ mang lại những thiết kế sang trọng và hiện đại.</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="single_chose">
+                    <div class="chose_icone">
+                        <img src="assets/img/about/About_icon3.png" alt="">
+                    </div>
+                    <div class="chose_content">
+                        <h3>Chất lượng cao</h3>
+                        <p>Mang lại những sản phẩm chất lượng tốt nhất.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--chose us area end-->
+
+<!--team area start-->
+<div class="team_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <article class="team_member">
+                    <figure>
+                        <div class="team_thumb">
+                            <img src="assets/img/about/z4944915731752_542e9303abb12eed43ffff154ab4bedf.jpg" alt="">
+                        </div>
+                        <figcaption class="team_content">
+                            <h3>Lê Duy Linh</h3>
+                            <p>SĐT: 0336107429 <br> Email: linhldph35320@fpt.edu.vn</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <article class="team_member">
+                    <figure>
+                        <div class="team_thumb">
+                            <img src="assets/img/about/z4944917211609_55d38188c2dea4780af5467dcecf7c07.jpg" alt="">
+                        </div>
+                        <figcaption class="team_content">
+                            <h3>Nguyễn Mạnh Thắng</h3>
+                            <p>SĐT: 0328632324 <br> Email: thangnmt888@gmail.com</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <article class="team_member">
+                    <figure>
+                        <div class="team_thumb">
+                            <img src="assets/img/about/z4944919816522_68b66e4900cc70b19c448d1c2f5cb36c.jpg" alt="">
+                        </div>
+                        <figcaption class="team_content">
+                            <h3>Phạm Ngọc Quý</h3>
+                            <p>SĐT: 0918113942 <br> Email: quypnph32494@fpt.edu.vn</p>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
